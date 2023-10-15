5f4b9fc34f3de7c0c5682f2dd273454a5031e9a2
Charly <cardan1990@hotmail.com>
2023-10-15 21:37:19 +0200

    Experimented with reactive forms for the login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  userEmailControl = new FormControl('');
+  userPasswordControl = new FormControl('');
 
+  submitForm() {
+    if (this.userEmailControl.dirty) {
+      alert('You changed the email field');
+    }
+  }
 }
